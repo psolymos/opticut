ae992eb5602b96dc5a66e68c2e32331e95386853
psolymos <psolymos@gmail.com>
2015-11-25 20:47:25 -0700

    plot

--- R/plots.R
+++ R/plots.R
@@ -4,9 +4,11 @@ wplot <- function (x, ...)
 
 ## plotting model weights, single species
 wplot.opticut1 <-
-function(x, cut=getOption("ocoptions")$cut, ylim=c(-1,1), las=1,
+function(x, cut, ylim=c(-1,1), las=1,
 ylab="Model weight * Association", xlab="Partitions", ...)
 {
+    if (missing(cut))
+        cut <- getOption("ocoptions")$cut
     w <- x$w * x$assoc
     names(w) <- rownames(x)
     if (!any(x$logLR >= cut)) {
@@ -33,10 +35,13 @@ ylab="Model weight * Association", xlab="Partitions", ...)
 
 ## plotting model weights, multi species
 wplot.opticut <-
-function(x, which=NULL,
-cut=getOption("ocoptions")$cut, sort=getOption("ocoptions")$sort, las=1,
+function(x, which=NULL, cut, sort, las=1,
 ylab="Model weight * Association", xlab="Partitions", ...)
 {
+    if (missing(cut))
+        cut <- getOption("ocoptions")$cut
+    if (missing(sort))
+        sort <- getOption("ocoptions")$sort
     if (!is.null(which) && length(which) == 1L) {
         wplot.opticut1(x$species[[which]],
             cut=cut, ylab=ylab, xlab=xlab, ...)
@@ -97,20 +102,34 @@ ylab="Model weight * Association", xlab="Partitions", ...)
     }
 }
 
-
 plot.opticut <-
-function(x, show_I=TRUE, show_S=TRUE, hr=TRUE,
-cut=getOption("ocoptions")$cut, sort=getOption("ocoptions")$sort, las=1,
-ylab="Relative abundance", xlab="Partitions",
+function(x, which = NULL, cut, sort,
+las=1, ylab="Relative abundance", xlab="Partitions",
+show_I=TRUE, show_S=TRUE, hr=TRUE,
 palette=colorRampPalette(c("blue","green","red")),
 mar=c(5, 4, 4, 4) + 0.1, ...)
 {
-    xx <- summary(x, cut=cut, sort=sort)
-    bp <- xx$highmat
-    iv <- xx$summary[,c("I", "mu0", "mu1")]
-    iv$h0 <- pmin(iv$mu0, iv$mu1) / (iv$mu0 + iv$mu1)
-    iv$h0[iv$h0 < 0.01] <- 0.01
-    iv$h1 <- pmax(iv$mu0, iv$mu1) / (iv$mu0 + iv$mu1)
+    if (missing(cut))
+        cut <- getOption("ocoptions")$cut
+    if (missing(sort))
+        sort <- getOption("ocoptions")$sort
+    ss <- summary(x)
+    xx <- ss$summary
+    bp <- ss$bestpart
+    if (sort) {
+        bp <- bp[attr(ss$bestpart, "row.order"),
+            attr(ss$bestpart, "col.order")]
+        xx <- xx[attr(ss$bestpart, "row.order"),]
+    }
+    if (!any(xx$logLR >= cut)) {
+        warning("All logLR < cut: cut ignored")
+    } else {
+        bp <- bp[xx$logLR >= cut, , drop=FALSE]
+        xx <- xx[xx$logLR >= cut, , drop=FALSE]
+    }
+    xx$h0 <- pmin(xx$mu0, xx$mu1) / (xx$mu0 + xx$mu1)
+    xx$h0[xx$h0 < 0.01] <- 0.01
+    xx$h1 <- pmax(xx$mu0, xx$mu1) / (xx$mu0 + xx$mu1)
     n <- nrow(bp)
     p <- ncol(bp)
     op <- par(las=las, mar=mar)
@@ -118,7 +137,7 @@ mar=c(5, 4, 4, 4) + 0.1, ...)
     plot(0, xlim=c(0, p), ylim=c(n, 0),
         type="n", axes=FALSE, ann=FALSE, ...)
     title(ylab=ylab, xlab=xlab)
-    axis(1, at=seq_len(ncol(bp))-0.5,
+    axis(1, at=seq_len(p)-0.5,
         labels=colnames(bp), tick=TRUE)
     axis(2, at=seq_len(n)-0.5,
         labels=rownames(bp), tick=TRUE)
@@ -127,23 +146,20 @@ mar=c(5, 4, 4, 4) + 0.1, ...)
             labels=colSums(bp), tick=FALSE)
     if (show_I)
         axis(4, at=seq_len(n)-0.5,
-            labels=format(round(iv$I, 2), nsmall = 2), tick=FALSE)
+            labels=format(round(xx$I, 2), nsmall=2), tick=FALSE)
     Cols <- palette(101)
     Br <- seq(0, 1, length=101)
     if (hr)
         abline(h=1:n-0.5, col=Cols[1L])
-    #abline(v=0:p, col=Cols[1L])
     for (i in seq_len(n)) {
         for (j in seq_len(p)) {
             h <- if (bp[i,j] == 1)
                 iv$h1[i] else iv$h0[i]
-            #Col <- grey(1-(h/1.2))
-            #Col <- grey(1-h)
             Col <- Cols[which.min(h >= Br)[1L]]
             polygon(c(0,1,1,0)+j-1, 0.45*c(-h,-h,h,h)+i-0.5,
                 col=Col, border=NA)
         }
     }
     box(col="grey")
-    invisible(x)
+    invisible(xx)
 }
