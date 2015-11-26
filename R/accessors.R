6ecea271969d19f20b673f5b98d62033ca38f790
psolymos <psolymos@gmail.com>
2015-11-25 19:22:06 -0700

    more files

---
+++ R/accessors.R
@@ -0,0 +1,174 @@
+bestpart <- function (object, ...)
+    UseMethod("bestpart")
+
+bestpart.opticut <-
+function (object, ...)
+{
+    out <- list()
+    if (object$comb == "rank") {
+        for (spp in names(object$species)) {
+            i <- rownames(object$species[[spp]])[which.max(object$species[[spp]]$logLR)]
+            out[[spp]] <- ifelse(object$strata %in% strsplit(i, " ")[[1L]], 1L, 0L)
+        }
+        out <- do.call(cbind, out)
+        rownames(out) <- object$strata
+    } else {
+        for (spp in names(object$species)) {
+            i <- rownames(object$species[[spp]])[which.max(object$species[[spp]]$logLR)]
+            out[[spp]] <- object$strata[,i]
+        }
+        out <- do.call(cbind, out)
+    }
+    out
+}
+
+.extractOpticut <-
+function (object, which=NULL, boot=FALSE,
+internal=TRUE, best=TRUE, ...)
+{
+    if (is.null(which))
+        which <- names(object$species)
+    bp <- bestpart(object)
+    spp <- names(object$species)
+    names(spp) <- spp
+    spp <- spp[which]
+    n <- NROW(object$Y)
+    if (is.logical(boot)) {
+        j <- if (boot)
+            sample.int(n, replace=TRUE) else seq_len(n)
+    } else {
+        j <- boot
+    }
+    out <- list()
+    for (i in spp) {
+        if (internal) {
+            if (!best)
+                stop("use best=TRUE when internal=TRUE")
+            out[[i]] <- .opticut1(
+                Y=object$Y[j,i],
+                X=object$X[j,],
+                Z1=bp[j,i],
+                dist=object$dist, ...)
+        } else {
+            if (best) {
+                out[[i]] <- opticut1(
+                    Y=object$Y[j,i,drop=TRUE],
+                    X=object$X[j,],
+                    Z=bp[j,i,drop=FALSE],
+                    dist=object$dist, ...)
+            } else {
+                zz <- object$strata
+                if (is.null(dim(zz))) {
+                    zz <- zz[j]
+                } else {
+                    zz <- zz[j,]
+                }
+                out[[i]] <- opticut1(
+                    Y=object$Y[j,i,drop=TRUE],
+                    X=object$X[j,],
+                    Z=zz,
+                    dist=object$dist, ...)
+            }
+        }
+    }
+    out
+}
+
+bestmodel <- function (object, ...)
+    UseMethod("bestmodel")
+
+bestmodel.opticut <-
+function (object, which=NULL, ...)
+{
+    .extractOpticut(object, which,
+        boot=FALSE,
+        internal=TRUE,
+        full_model=TRUE, ...)
+}
+
+uncertainty <- function (object, ...)
+    UseMethod("uncertainty")
+
+uncertainty.opticut <-
+function (object, which=NULL,
+type=c("asymp", "boot", "multi"), B=99, ...)
+{
+    type <- match.arg(type)
+    B <- as.integer(B)
+    if (B < 1)
+        stop("B must be > 0")
+    linkinv <- .opticut1(
+        Y=object$Y[,1L],
+        X=object$X,
+        Z1=NULL,
+        dist=object$dist, ...)$linkinv
+    m <- .extractOpticut(object, which,
+        boot=FALSE,
+        internal=TRUE,
+        full_model=TRUE,
+        best=TRUE, ...)
+    spp <- names(m)
+    out <- list()
+    if (type == "asymp") {
+        for (i in spp) {
+            k <- which.max(object$species[[i]]$logLR)
+            bm <- rownames(object$species[[i]])[k]
+            m1 <- m[[i]]
+            cf <- MASS::mvrnorm(B, coef(m1), vcov(m1))[,c(1L, 2L)]
+            cf <- rbind(coef(m1)[c(1L, 2L)], cf)
+            cf0 <- linkinv(cf[,1L])
+            cf1 <- linkinv(cf[,1L] + cf[,2L])
+            I <- 1 - (pmin(cf0, cf1) / pmax(cf0, cf1))
+            out[[i]] <- data.frame(best=bm, I=I, mu0=cf0, mu1=cf1)
+        }
+    }
+    if (type == "boot") {
+        for (i in spp) {
+            k <- which.max(object$species[[i]]$logLR)
+            bm <- rownames(object$species[[i]])[k]
+            cf <- t(pbapply::pbsapply(seq_len(B), function(z) {
+                .extractOpticut(object, i,
+                    boot=TRUE,
+                    internal=TRUE,
+                    full_model=FALSE,
+                    best=TRUE, ...)[[1L]]$coef[c(1L, 2L)]
+            }))
+            cf <- rbind(coef(m[[i]])[c(1L, 2L)], cf)
+            cf0 <- linkinv(cf[,1L])
+            cf1 <- linkinv(cf[,1L] + cf[,2L])
+            I <- 1 - (pmin(cf0, cf1) / pmax(cf0, cf1))
+            out[[i]] <- data.frame(best=bm, I=I, mu0=cf0, mu1=cf1)
+        }
+    }
+    if (type == "multi") {
+        for (i in spp) {
+            if (object$comb == "all")
+                stop("comb='all' is no good, use 'rank' instead")
+            k <- which.max(object$species[[i]]$logLR)
+            bm <- character(B + 1L)
+            bm[1L] <- rownames(object$species[[i]])[k]
+            mat <- matrix(NA, B + 1L, 3)
+            colnames(mat) <- c("I", "mu0", "mu1")
+            tmp <- as.numeric(object$species[[i]][k, -1L])
+            names(tmp) <- colnames(object$species[[i]])[-1L]
+            mat[1L, ] <- tmp[c("I", "mu0", "mu1")]
+            pb <- pbapply::startpb(0, B)
+            on.exit(pbapply::closepb(pb))
+            for (j in seq_len(B)) {
+                ## Z is factor, thus 'rank' applied
+                mod <- .extractOpticut(object, i,
+                    boot=TRUE,
+                    internal=FALSE,
+                    best=FALSE, ...)[[1L]]
+                k <- which.max(mod$logLR)
+                bm[j + 1L] <- rownames(mod)[k]
+                tmp <- as.numeric(mod[k, -1L])
+                names(tmp) <- colnames(mod)[-1L]
+                mat[j + 1L, ] <- tmp[c("I", "mu0", "mu1")]
+                pbapply::setpb(pb, j)
+            }
+            out[[i]] <- data.frame(best=bm, mat)
+        }
+    }
+    out
+}
---
+++ R/comb.R
@@ -0,0 +1,121 @@
+## -------------- OptiCut -----------
+
+## higher than kmax is complement,
+## e.g. 100 is same as 011 for our purposes
+## this returns a 'contrast' matrix corresponding to
+## all possible binary partitions of the factor levels n
+kComb <-
+function(k)
+{
+    k <- as.integer(k)
+    if (k < 2)
+        stop("k must be at least 2")
+    kmax <- floor(k/2)
+    s <- seq_len(k)
+    clist <- lapply(seq_len(kmax), function(kk) combn(k, kk))
+    ## if kmax is even, take care of cases like
+    ## 1100 and 0011
+    if (kmax == k/2) {
+        COL <- seq_len(ncol(clist[[kmax]])/2)
+        clist[[kmax]] <- clist[[kmax]][,COL, drop=FALSE]
+    }
+    m <- sapply(clist, ncol)
+    out <- matrix(0L, k, sum(m))
+    z <- 1
+    for (i in seq_len(length(clist))) {
+        for (j in seq_len(m[i])) {
+            out[s %in% clist[[i]][,j],z] <- 1L
+            z <- z + 1
+        }
+    }
+    out
+}
+
+## this takes a classification vector
+## with at least 2 levels
+## and returns a model matrix with binary partitions
+allComb <-
+function(x, collapse = getOption("ocoptions")$collapse)
+{
+    f <- droplevels(as.factor(x))
+    LEVELS <- gsub("\\s", "", levels(f))
+    i <- as.integer(f)
+    n <- max(i)
+    s <- seq_len(n)
+    ac <- kComb(n)
+    LABELS <- apply(ac, 2, function(z)
+        paste(LEVELS[as.logical(z)], collapse=collapse))
+    out <- apply(ac, 2, function(z) z[match(i, s)])
+    rownames(out) <- f
+    colnames(out) <- LABELS
+    out
+}
+
+## this checks a design matrix for complementary rows
+## e.g. 1100 vs 0011
+checkComb <- function(x) {
+    n <- NCOL(x)
+    if (n < 2)
+        return(TRUE)
+    mat <- matrix(FALSE, n, n)
+    for (i in 1:(n-1)) {
+        for (j in (i+1):n) {
+            ## upper.tri
+            mat[i,j] <- all(x[,i] == 1-x[,j]) # comp(lementary)
+            ## lower.tri
+            mat[j,i] <- all(x[,i] == x[,j]) # same
+        }
+    }
+    out <- !any(mat)
+    attr(out, "comp") <- cbind(
+        i=row(mat)[upper.tri(row(mat))][which(mat[upper.tri(mat)])],
+        j=col(mat)[upper.tri(col(mat))][which(mat[upper.tri(mat)])])
+    attr(out, "same") <- cbind(
+        i=row(mat)[lower.tri(row(mat))][which(mat[lower.tri(mat)])],
+        j=col(mat)[lower.tri(col(mat))][which(mat[lower.tri(mat)])])
+    out
+}
+
+
+## x is a named vector of ranks, referring to factor levels
+## in some classification vector, 1=highest abundance.
+oComb <-
+function(x, collapse = getOption("ocoptions")$collapse)
+{
+    if (length(x) < 2L)
+        stop("length of x must be >1")
+    if (is.null(names(x)))
+        names(x) <- seq_len(length(x))
+    o <- x[order(x, decreasing = FALSE)]
+    out <- diag(1L, length(o))
+    out[upper.tri(out)] <- 1L
+    out <- out[,-ncol(out)]
+    rownames(out) <- names(o)
+    colnames(out) <- seq_len(ncol(out))
+    for (i in seq_len(ncol(out))) {
+        colnames(out)[i] <- paste(names(x)[names(x) %in%
+            rownames(out)[out[,i] > 0]],
+            collapse = collapse)
+    }
+    #out <- 1L - out
+    attr(out, "rank") <- o
+    out
+}
+
+rankComb <-
+function(Y, X, Z, dist="gaussian",
+collapse = getOption("ocoptions")$collapse, ...)
+{
+    if (!is.factor(Z))
+        stop("Z must be a factor")
+    Z0 <- model.matrix(~Z)
+    m <- .opticut1(Y, X, Z1=Z0[,-1L,drop=FALSE],
+        linkinv=TRUE, dist=dist, ...)
+    lc <- c(m$coef[1], m$coef[1] + m$coef[2:ncol(Z0)])
+    names(lc) <- levels(Z)
+    x <- rank(-lc)
+    oc <- oComb(x, collapse = collapse)
+    out <- oc[match(Z, rownames(oc)),]
+    attr(out, "est") <- m$linkinv(lc)
+    out
+}
