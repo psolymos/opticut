dca2b4b56a2a8de2debb77ba135df4501abf720f
psolymos <psolymos@gmail.com>
2015-11-29 10:10:30 -0700

    failsafe

--- R/summary.R
+++ R/summary.R
@@ -107,7 +107,8 @@ summary.opticut <- function(object, ...)
     res$assoc <- .parseAssoc(res)
     res$logL <- NULL
     bp <- bestpart(object)
-    bp <- mefa4::nonDuplicated(bp, rownames(bp), TRUE)
+    #bp <- mefa4::nonDuplicated(bp, rownames(bp), TRUE)
+    bp <- bp[!duplicated(rownames(bp)),,drop=FALSE]
     sgn <- sign(c(-3, -2, -1, 0, 1, 2, 3)[as.integer(res$assoc)])
     lab1 <- character(ncol(bp))
     lab0 <- character(ncol(bp))
