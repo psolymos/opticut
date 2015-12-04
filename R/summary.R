5c33181fe6e58cfe967284ef36e95c32219c4fa7
Peter Solymos <psolymos@gmail.com>
2015-12-04 13:28:01 -0700

    revised print.summary

--- R/summary.R
+++ R/summary.R
@@ -83,10 +83,14 @@ print.summary.opticut <- function(x, cut, sort, digits, ...) {
     }
     xx <- xx[xx$logLR >= cut, , drop=FALSE]
     Missing <- nrow(x$summary) - nrow(xx)
+    tmp <- if (nrow(xx) > 1L)
+        "Best supported models" else "Best supported model"
+    TXT <- paste0(tmp, " with logLR >= ",
+        format(cut, digits = digits), ":")
     cat("Multivariate opticut results, comb = ", x$comb, ", dist = ", x$dist,
         "\n", sep="")
     cat("\nCall:\n", paste(deparse(x$call), sep = "\n", collapse = "\n"),
-            "\n\n", sep = "")
+            "\n\n", TXT, "\n", sep = "")
     print(format.data.frame(xx, digits=digits), ...)
     cat(x$nsplit, "binary", ifelse(x$nsplit > 1, "splits\n", "split\n"))
     if (Missing)
