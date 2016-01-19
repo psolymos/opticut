45ed1d9f053c06ccd58284f022d0c80c7a201781
Peter Solymos <psolymos@gmail.com>
2016-01-19 12:06:08 -0700

    misleading comment

--- R/ocoptions.R
+++ R/ocoptions.R
@@ -28,7 +28,7 @@ function(theme)
             "br" = c("#2c7bb6", "#abd9e9", "#ffffbf",
                 "#fdae61", "#d7191c"), # bu-yl-rd
             "gr" = c("#1a9641", "#a6d96a", "#ffffbf",
-                "#fdae61", "#d7191c"), # bu-yl-rd
+                "#fdae61", "#d7191c"), # gr-yl-rd
             "bw" = c("#FFFFFF", "#BFBFBF", "#808080",
                 "#404040", "#000000")) # bw
     }
