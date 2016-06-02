c2ef1a5198b6c35b5c0f4494fd50c110d8d07f07
Peter Solymos <psolymos@gmail.com>
2016-06-01 20:50:48 -0600

    Revert "throbber as default"
    This reverts commit bcaec66d80812d261a30408cf5596779b4dc1b5e.

--- R/zzz.R
+++ R/zzz.R
@@ -10,14 +10,11 @@
             theme="br",
             check_comb=TRUE,
             try_error=FALSE))
-    .pboptions_old_vals <<- pboptions(style=4)
     invisible(NULL)
 }
 
 .onUnload <- function(libpath){
     options("ocoptions"=NULL)
-    pboptions(.pboptions_old_vals)
-    rm(.pboptions_old_vals)
     invisible(NULL)
 }
 
