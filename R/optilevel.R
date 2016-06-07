4815f3db96d0297adb88082a204823effcbe1145
Peter Solymos <psolymos@gmail.com>
2016-06-06 22:57:59 -0600

    optilevel can take confounding as z

--- R/optilevel.R
+++ R/optilevel.R
@@ -8,7 +8,7 @@
 ## estimation
 
 .optilevel <-
-function(Y, X, alpha=0, dist="gaussian", ...)
+function(Y, X, Z = NULL, alpha=0, dist="gaussian", ...)
 {
     if (!is.function(dist)) {
         dist <- match.arg(dist,
@@ -20,24 +20,42 @@ function(Y, X, alpha=0, dist="gaussian", ...)
             stop("dist='rsf' not supported for optilevel")
     }
     if (is.null(colnames(X)))
-        colnames(X) <- paste0("V", seq_len(ncol(X)))
+        colnames(X) <- paste0("X", seq_len(ncol(X)))
     colnames(X) <- gsub("\\s", "", colnames(X))
+#    nx <- ncol(X)
+    if (!is.null(Z)) {
+        Z <- data.matrix(Z)
+        if (is.null(colnames(Z)))
+            colnames(Z) <- paste0("Z", seq_len(ncol(Z)))
+        colnames(Z) <- gsub("\\s", "", colnames(Z))
+        nz <- ncol(Z)
+        X <- cbind(X, Z)
+    } else {
+        nz <- 0
+    }
 
     m_full <- .opticut1(Y, X, Z1=NULL, dist=dist, ...)
     cf_full <- m_full$coef
     if (dist %in% c("zip","zinb","beta"))
         cf_full <- cf_full[-length(cf_full)]
+
+    cf_full_z <- cf_full[(length(cf_full) - nz + 1):length(cf_full)]
+    cf_full <- cf_full[1:(length(cf_full) - nz)]
+
     rnk_full <- rank(cf_full)
     k <- length(cf_full)
     n <- length(Y)
-    AIC_m_full <- -2*m_full$logLik + 2*k
-    BIC_m_full <- -2*m_full$logLik + log(n)*k
+    AIC_m_full <- -2*m_full$logLik + 2*(k+nz)
+    BIC_m_full <- -2*m_full$logLik + log(n)*(k+nz)
     IC_full <- (1-alpha)*AIC_m_full + alpha*BIC_m_full
     cfmat <- matrix(NA, k, k)
     cfmat[1,] <- cf_full
+    cfzmat <- matrix(NA, k, nz)
+    cfzmat[1,] <- cf_full_z
     rnkmat <- matrix(NA, k, k)
     rnkmat[1,] <- rnk_full
-    colnames(rnkmat) <- colnames(cfmat) <- colnames(X)
+    colnames(rnkmat) <- colnames(cfmat) <- colnames(X)[1:(ncol(X) - nz)]
+    colnames(cfzmat) <- colnames(Z)
     delta <- rep(NA, k)
     delta[1] <- 0
     ICvec <- rep(NA, k)
@@ -48,9 +66,11 @@ function(Y, X, alpha=0, dist="gaussian", ...)
     delta_list <- list()
     IC_list <- list()
     cfmat_list <- list()
+    cfzmat_list <- list()
     rnkmat_list <- list()
     while (Delta < 0) {
         cfmat_list[[j]] <- matrix(NA, k-j, k)
+        cfzmat_list[[j]] <- matrix(NA, k-j, nz)
         rnkmat_list[[j]] <- matrix(NA, k-j, k)
         delta_list[[j]] <- numeric(k-j)
         IC_list[[j]] <- numeric(k-j)
@@ -60,21 +80,28 @@ function(Y, X, alpha=0, dist="gaussian", ...)
             l1 <- unique(gr[rnk == i])
             l2 <- unique(gr[rnk == i+1])
             gr[gr %in% c(l1, l2)] <- paste(l1, l2, sep="+")
-            XX <- mefa4::groupSums(X, 2, gr)
+            XX <- mefa4::groupSums(X[,1:(ncol(X) - nz),drop=FALSE], 2, gr)
+            if (!is.null(Z))
+                XX <- cbind(XX, Z)
 
             m <- .opticut1(Y, XX, Z1=NULL, dist=dist, ...)
             cf <- m$coef
             if (dist %in% c("zip","zinb","beta"))
                 cf <- cf[-length(cf_full)]
+
+            cf_z <- cf[(length(cf) - nz + 1):length(cf)]
+            cf <- cf[1:(length(cf) - nz)]
+
             rnk <- rank(cf)
             kk <- length(cf)
-            AIC_m <- -2*m$logLik + 2*kk
-            BIC_m <- -2*m$logLik + log(n)*kk
+            AIC_m <- -2*m$logLik + 2*(kk+nz)
+            BIC_m <- -2*m$logLik + log(n)*(kk+nz)
             IC <- (1-alpha)*AIC_m + alpha*BIC_m
             IC_list[[j]][i] <- IC
             delta_list[[j]][i] <- IC - IC_best
-            names(cf) <- names(rnk) <- colnames(XX)
+            names(cf) <- names(rnk) <- colnames(XX)[1:(ncol(XX) - nz)]
             cfmat_list[[j]][i,] <- cf[gr]
+            cfzmat_list[[j]][i,] <- cf_z
             rnkmat_list[[j]][i,] <- rnk[gr]
         }
         best <- which.min(delta_list[[j]])
@@ -84,19 +111,23 @@ function(Y, X, alpha=0, dist="gaussian", ...)
             ICvec[j+1] <- IC_list[[j]][best]
             delta[j+1] <- Delta
             cfmat[j+1,] <- cfmat_list[[j]][best,]
+            cfzmat[j+1,] <- cfzmat_list[[j]][best,]
             rnkmat[j+1,] <- rnkmat_list[[j]][best,]
         }
         j <- j + 1
         if (j >= k)
             break
     }
-    list(delta=delta, ic=ICvec, coef=cfmat, rank=rnkmat,
+    list(delta=delta, ic=ICvec,
+        coef=cfmat, zcoef=if (is.null(Z)) Z else cfzmat,
+        rank=rnkmat,
         deltalist=delta_list, iclist=IC_list,
-        coeflist=cfmat_list, ranklist=rnkmat_list)
+        coeflist=cfmat_list, zcoeflist=if (is.null(Z)) Z else cfzmat_list,
+        ranklist=rnkmat_list)
 }
 
 optilevel <-
-function(y, x, alpha=0, dist="gaussian", ...)
+function(y, x, z = NULL, alpha=0, dist="gaussian", ...)
 {
     if (is.null(dim(x))) {
         if (!is.factor(x))
@@ -110,11 +141,11 @@ function(y, x, alpha=0, dist="gaussian", ...)
             stop("zombie (sum=0) columns in x")
         X <- as.matrix(x)
     }
-    out <- .optilevel(Y=y, X=X, alpha=alpha, dist=dist, ...)
+    out <- .optilevel(Y=y, X=X, Z=z, alpha=alpha, dist=dist, ...)
     levs <- list()
     for (i in seq_len(length(out$ranklist))) {
         levi <- sapply(1:max(out$rank[i,]), function(j)
-            paste(colnames(out$coef)[out$rank[i,] == j], 
+            paste(colnames(out$coef)[out$rank[i,] == j],
             collapse=getOption("ocoptions")$collapse))
         levs[[i]] <- levi[out$rank[i,]]
         names(levs[[i]]) <- colnames(out$coef)
