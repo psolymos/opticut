4b61ecf3562442a7582ea013274eb1623e8e430c
Peter Solymos <psolymos@gmail.com>
2016-06-07 23:42:48 -0600

    working version: needs testing

--- R/optilevel.R
+++ R/optilevel.R
@@ -153,6 +153,30 @@ function(y, x, z = NULL, alpha=0, dist="gaussian", ...)
     }
     out$levels <- levs
     out$factor <- is.factor(x)
+    out$call <- match.call()
     class(out) <- "optilevel"
     out
 }
+
+bestmodel.optilevel <-
+function (object, ...)
+{
+    ## need to combine X and Z for best model
+    ## factor vs matrix: all the same
+    xg <- mefa4::groupSums(object$X, 2,
+        object$levels[[length(object$levels)]])
+    object$X <- cbind(xg, object$Z)
+    CALL <- object$call
+    CALL[[1]] <- as.name(".opticut1")
+    CALL$y <- NULL
+    CALL$x <- NULL
+    CALL$z <- NULL
+    CALL$alpha <- NULL
+#    CALL$dist <- NULL
+    CALL$Y <- as.name("Y")
+    CALL$X <- as.name("X")
+    CALL$full_model <- TRUE
+    out <- eval(CALL, envir=object)
+    out
+}
+
