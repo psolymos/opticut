64adb1bf61fcfebe0b536ab671a495de06585649
Peter Solymos <psolymos@gmail.com>
2016-09-13 22:54:48 -0600

    pb options

--- R/zzz.R
+++ R/zzz.R
@@ -10,6 +10,16 @@
             theme="br",
             check_comb=TRUE,
             try_error=FALSE))
+    options("pboptions" = list(
+        type = if (interactive()) "timer" else "none",
+        char = "+",
+        txt.width = 50,
+        gui.width = 300,
+        style = 4,
+        initial = 0,
+        title = "R progress bar",
+        label = "",
+        nout = 100L))
     invisible(NULL)
 }
 
