90f2e742183e0cb73ce2f5de3474e63a6e13e9ea
Peter Solymos <psolymos@gmail.com>
2016-10-04 12:04:55 -0600

    ocut

---
+++ R/opticut1.R
@@ -0,0 +1,99 @@
+## Y is abundance vector
+## X is model matrix for nuisance variables
+## Z is design matrix for binary splits or a factor (using rankComb)
+opticut1 <-
+function(Y, X, Z, dist="gaussian", sset=NULL, ...)
+{
+    if (missing(X))
+        X <- matrix(1, length(Y), 1)
+    X <- data.matrix(X)
+    if (is.null(rownames(X)))
+        rownames(X) <- seq_len(nrow(X))
+    if (is.factor(Z)) {
+        Z <- rankComb(Y, X, Z, dist=dist, ...)
+        Est <- attr(Z, "est")
+        Comb <- "rank"
+    } else {
+        Est <- NA
+        Comb <- attr(Z, "comb")
+        if (is.null(Comb))
+            Comb <- NA
+    }
+    Z <- data.matrix(Z)
+    if (is.null(colnames(Z)))
+        colnames(Z) <- paste0("split.", seq_len(ncol(Z)))
+    if (getOption("ocoptions")$check_comb && !checkComb(Z))
+        stop("Guess what! Complementary design variables found:\nuse 'checkComb'")
+    if (length(unique(c(length(Y), nrow(X), nrow(Z)))) > 1)
+        stop("Dimension mismatch: check you input.")
+    if (is.null(rownames(Z))) {
+        warning("Row names added to binary split matrix Z (it was NULL). You are welcome.")
+        rownames(Z) <- apply(Z, 1, paste, collapse="")
+    }
+    N <- ncol(Z)
+    if (!is.null(sset)) {
+        Y <- Y[sset]
+        X <- X[sset,,drop=FALSE]
+        Z <- Z[sset,,drop=FALSE]
+    }
+    res0 <- .opticut1(Y, X, Z1=NULL, dist=dist, ...)
+    cf <- matrix(0, N, length(res0$coef)+1)
+    rownames(cf) <- colnames(Z)
+    ll <- numeric(N)
+    names(ll) <- colnames(Z)
+    for (i in seq_len(N)) {
+        res <- .opticut1(Y, X, Z1=Z[,i], dist=dist, ...)
+        cf[i,] <- res$coef
+        ll[i] <- res$logLik
+    }
+    dll <- ll - max(ll)
+    w <- exp(dll) / sum(exp(dll))
+    cfnull <- res0$linkinv(res0$coef[1L])
+    cf0 <- res0$linkinv(cf[,1L])
+    cf1 <- res0$linkinv(cf[,1L] + cf[,2L])
+    h <- sign(cf[,2L])
+
+    ## AIC weight has a penalty dependent 'midpoint'
+    ## only 1 df difference, this length of coef is not important
+    #ic <- cbind(ic = -2*ll + getOption("ocoptions")$penalty,
+    #    ic_null = -2*res0$logLik)
+    #Delta <- t(apply(ic, 1, function(z) z - min(z)))
+    #W <- apply(Delta, 1, function(z) exp(-0.5*z[1]) / sum(exp(-0.5*z)))
+    ## delta IC with inverse Fisher transform is more intuitive
+    ## 0 when there is no support for a partition
+    ## 1 when logLR is HUGE
+    ## this is trasformed (AIC_null - AIC_m)
+    #W <- pmax(0, tanh(2*ll - 2*res0$logLik - getOption("ocoptions")$penalty))
+
+    ## problem with prediction based I is that
+    ## - it depends on covariates if not centered
+    ## - ordered intercept can be ill-defined in some cases
+#    I <- 1 - (pmin(cf0, cf1) / pmax(cf0, cf1))
+#    if (any(cf0 < 0) || any(cf1 < 0)) {
+#        warning("Negative prediction: I-value set to NA")
+#        I[I < 0 | I > 1] <- NA
+#    }
+    ## we want model based I (i.e. not mean(Y|z))
+    ## thus the 0-1 rescaled version of \beta_1
+    ## this is independent from linkinv scaling (and covariates in some sense)
+    ## thus comparable across species AND across studies
+#    I <- tanh(abs(cf[,2L]))
+
+    out <- data.frame(assoc=h,
+        I=tanh(abs(cf[,2L])),
+#        I=2*(plogis(abs(cf[,2L]))-0.5),
+        null=cfnull,
+        mu0=cf0, mu1=cf1,
+        beta0=cf[,1L], beta1=cf[,2L],
+        logL=ll, logLR=ll-res0$logLik, w=w)
+    rownames(out) <- colnames(Z)
+    attr(out, "logL_null") <- res0$logLik
+#    attr(out, "penalty") <- getOption("ocoptions")$penalty
+    attr(out, "H") <- sum(w^2)
+    attr(out, "dist") <- if (is.function(dist))
+        deparse(substitute(dist)) else dist
+    attr(out, "comb") <- Comb
+    attr(out, "est") <- Est
+    class(out) <- c("opticut1", "data.frame")
+    out
+}
---
+++ RRR/opticut1dot.R
@@ -0,0 +1,148 @@
+## Z1 is:
+## * NULL (this is used to fit model for H0, i.e. no partition)
+## * a single column from Z matrix
+## * or a matrix itself (design matrix w/o intercept)
+.opticut1 <-
+function(Y, X, Z1=NULL,
+dist="gaussian", linkinv, full_model=FALSE, ...)
+{
+    if (missing(linkinv))
+        linkinv <- is.null(Z1)
+    if (is.null(Z1)) {
+        XX <- as.data.frame(X)
+    } else {
+        if (is.null(dim(X)))
+            X <- data.matrix(X)
+        XX <- as.data.frame(cbind(X[,1,drop=FALSE], Z1, X[,-1,drop=FALSE]))
+    }
+    if (!is.function(dist)) {
+        dist <- as.character(dist)
+        dist <- strsplit(dist, ":", fixed=TRUE)[[1]]
+        if (length(dist) > 1L) {
+            link <- dist[2L]
+            dist <- dist[1L]
+        } else {
+            link <- NULL
+        }
+        dist <- match.arg(dist,
+            c("gaussian","poisson","binomial","negbin",
+            "beta","zip","zinb","ordered", "rsf", "rspf",
+            "zip2", "zinb2"))
+        if (dist %in% c("gaussian", "poisson", "binomial")) {
+            if (is.null(link))
+                link <- switch(dist,
+                    "gaussian"="identity",
+                    "poisson"="log",
+                    "binomial"="logit")
+            Family <- switch(dist,
+                "gaussian"=gaussian(link),
+                "poisson"=poisson(link),
+                "binomial"=binomial(link))
+            mod <- stats::glm(Y ~ .-1, data=XX, family=Family, ...)
+            cf <- coef(mod)
+            ll <- as.numeric(logLik(mod))
+            linv <- family(mod)$linkinv
+        }
+        if (dist == "negbin") {
+            mod <- MASS::glm.nb(Y ~ .-1, data=XX, ...)
+            cf <- coef(mod)
+            ll <- as.numeric(logLik(mod))
+            linv <- family(mod)$linkinv
+        }
+        if (dist == "beta") {
+            mod <- betareg::betareg(Y ~ .-1, data=XX, ...)
+            cf <- coef(mod)
+            ll <- as.numeric(logLik(mod))
+            linv <- mod$link$mean$linkinv
+        }
+        if (dist %in% c("zip", "zinb")) {
+            if (is.null(link))
+                link <- "logit"
+            Dist <- switch(dist,
+                "zip"="poisson",
+                "zinb"="negbin")
+            mod <- pscl::zeroinfl(Y ~ .-1 | 1, data=XX, dist=Dist,
+                link=link, ...)
+            cf <- coef(mod)
+            ll <- as.numeric(logLik(mod))
+            linv <- mod$linkinv
+        }
+        if (dist %in% c("zip2", "zinb2")) {
+            ZZ <- if (is.null(Z1)) {
+                data.matrix(X[,1,drop=FALSE])
+            } else {
+                data.matrix(cbind(X[,1,drop=FALSE], Z1))
+            }
+            if (is.null(link))
+                link <- "logit"
+            Dist <- switch(dist,
+                "zip2"="poisson",
+                "zinb2"="negbin")
+            mod <- pscl::zeroinfl(Y ~ X-1 | ZZ-1, dist=Dist,
+                link=link, ...)
+            cf <- c(coef(mod, "zero"), coef(mod, "count"))
+            ll <- as.numeric(logLik(mod))
+            linv <- function(eta) 1 - binomial(link)$linkinv(eta)
+        }
+        if (dist == "ordered") {
+            if (!is.null(list(...)$method))
+                if (list(...)$method != "logistic")
+                    stop("Sorry but only logisic model allowed for ordered.")
+            if (!is.null(link))
+                if (link != "logistic")
+                    stop("Sorry but only logisic model allowed for ordered.")
+            Y <- as.ordered(Y)
+            if (nlevels(Y) > 2) { # ordinal
+                ## need to keep the intercept
+                if (ncol(XX)==1) {
+                    if (!is.null(list(...)$data))
+                        stop("Note: data argument should not be provided as part of ...")
+                    mod <- MASS::polr(Y ~ 1, method="logistic", ...)
+                } else {
+                    mod <- MASS::polr(Y ~ ., data=data.frame(XX[,-1,drop=FALSE]),
+                        method="logistic", ...)
+                }
+                cf <- c(mod$zeta[1], coef(mod))
+            } else {
+                mod <- stats::glm(Y ~ .-1, data=XX, family=binomial("logit"), ...)
+                cf <- coef(mod)
+            }
+            ll <- as.numeric(logLik(mod))
+            linv <- binomial("logit")$linkinv
+        }
+        if (dist %in% c("rsf", "rspf")) {
+            m <- list(...)$m
+            if (dist == "rsf") {
+                if (is.null(m))
+                    stop("'m' must be provided, have you checked help('rsf') ?")
+                if (!is.null(link) && link != "log")
+                    warning("link argument ignored for dist='rsf' (log link used by default)")
+                link <- "log" # this is needed for linkinv below
+                ## note: the call uses link='log', no need to provide link=link here!
+                mod <- ResourceSelection::rsf(Y ~ ., data=XX[,-1,drop=FALSE], ...)
+                ## intercept is not reported by rsf
+                ## and this can cause problems in X %*% theta
+                cf <- c(0, mod$coefficients)
+            } else {
+                if (is.null(m))
+                    stop("'m' must be provided, have you checked help('rspf') ?")
+                if (is.null(link))
+                    link <- "logit" # this is needed for linkinv below
+                mod <- ResourceSelection::rspf(Y ~ ., data=XX[,-1,drop=FALSE],
+                    link=link, ...)
+                cf <- mod$coefficients
+            }
+            ll <- as.numeric(mod$loglik)
+            linv <- binomial(link)$linkinv
+        }
+        if (!linkinv)
+            linv <- NULL
+        out <- list(coef=cf, logLik=ll, linkinv=linv)
+    } else {
+        if (full_model)
+            stop("Unable to return full model for custom distribution function.")
+        out <- dist(Y, XX, linkinv, ...)
+    }
+    if (full_model)
+        mod else out
+}
