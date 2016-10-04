c99fda03b5d619a09d67b6b6cb74fbd6b2c33f30
Peter Solymos <psolymos@gmail.com>
2016-10-04 10:50:40 -0600

    oops

--- R/zzz.R
+++ R/zzz.R
@@ -3,14 +3,14 @@
                     fields=c("Version", "Date"))
     packageStartupMessage(paste(pkgname, ver[1], "\t", ver[2]))
     if (is.null(getOption("ocoptions")))
-        options("ocoptions"=list(
+        options("ocoptions" = list(
             collapse=" ",
             cut=2,
             sort=TRUE,
             theme="br",
             check_comb=TRUE,
             try_error=FALSE))
-    opts <- list(
+    options("pboptions" = list(
         type = if (interactive()) "timer" else "none",
         char = "=",
         txt.width = 50,
@@ -24,7 +24,7 @@
 }
 
 .onUnload <- function(libpath){
-    options("ocoptions"=NULL)
+    options("ocoptions" = NULL)
     invisible(NULL)
 }
 
