bec29afdd244219ab2facd77872bcfd73f10dc0b
Peter Solymos <psolymos@gmail.com>
2016-10-04 23:35:58 -0600

    documentations

--- R/print.summary.uncertainty.R
+++ R/print.summary.uncertainty.R
@@ -1,13 +1,15 @@
 ## do not cut, but sort???
 print.summary.uncertainty <-
-function(x, ...)
+function(x, digits, ...)
 {
+    if (missing(digits))
+        digits <- max(3L, getOption("digits") - 3L)
     cat("Multivariate opticut uncertainty results",
         ", type = ", x$type, ", B = ", x$B,
         "\nlevel = ", format(round(x$level, 2), nsmall=2),
         "\n\n", sep="")
     uct <- x$uctab
     uct <- uct[order(uct$split, -uct$R, -uct$I),]
-    print(uct, ...)
+    print(format.data.frame(uct, digits=digits), ...)
     invisible(x)
 }
