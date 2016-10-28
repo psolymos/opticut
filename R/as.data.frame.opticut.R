036eb1dd2411ee8c0027709165bdd4ddbdd7aaf8
Peter Solymos <psolymos@gmail.com>
2016-10-28 12:15:24 -0600

    infinite coercion -- not good

--- R/as.data.frame.opticut.R
+++ R/as.data.frame.opticut.R
@@ -1,6 +1,6 @@
 as.data.frame.opticut <-
 function(x, row.names = NULL, optional = FALSE, cut, sort, ...)
 {
-    as.data.frame(as.data.frame(x, cut=cut, sort=sort),
-        row.names=row.names, optional=optional, ...)
+    as.data.frame(summary(x), row.names=row.names, optional=optional,
+        cut=cut, sort=sort, ...)
 }
