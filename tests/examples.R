ea2accb0b3b0ea79ddbbba64d3de7a38d1411ea8
Peter Solymos <psolymos@gmail.com>
2016-10-28 11:27:55 -0600

    testing

--- tests/examples.R
+++ tests/examples.R
@@ -1,5 +1,7 @@
 library(opticut)
 
+## run examples, even \dontrun sections
+
 help_pages <- c("opticut-package",
     "dolina",
     "opticut", "optilevels", "sindex",
@@ -12,3 +14,30 @@ for (i in help_pages) {
     eval(parse(text=paste0("example(", i,
         ", package = 'opticut', run.dontrun = TRUE)")))
 }
+
+## testing coercion methods
+
+set.seed(2345)
+n <- 50
+x0 <- sample(1:4, n, TRUE)
+x1 <- ifelse(x0 %in% 1:2, 1, 0)
+x2 <- rnorm(n, 0.5, 1)
+x3 <- ifelse(x0 %in% 2:4, 1, 0)
+lam1 <- exp(0.5 + 1*x1 + -0.2*x2)
+Y1 <- rpois(n, lam1)
+lam2 <- exp(1 + 0.5*x3)
+Y2 <- rpois(n, lam2)
+Y3 <- rpois(n, exp(0))
+Y <- cbind(Spp1=Y1, Spp2=Y2, Spp3=Y3)
+oc <- opticut(Y ~ x2, strata=x0, dist="poisson", comb="rank")
+uc <- uncertainty(oc, type="asymp", B=999)
+
+#as.matrix(oc)
+#as.matrix(summary(oc))
+#as.data.frame(oc)
+#as.data.frame(summary(oc))
+
+as.matrix(uc)
+as.matrix(summary(uc))
+as.data.frame(uc)
+as.data.frame(summary(uc))
