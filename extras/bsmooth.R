65ac6f9d27bd184ab5751fd5145053f3c8bf6879
Peter Solymos <psolymos@gmail.com>
2016-11-21 16:38:56 -0700

    extra

--- extras/bsmooth.R
+++ extras/bsmooth.R
@@ -1,12 +1,10 @@
+library(opticut)
+
 data(dolina)
-## stratum as ordinal
-dolina$samp$stratum <- as.integer(dolina$samp$stratum)
-## filter species to speed up things a bit
-Y <- ifelse(dolina$xtab[,colSums(dolina$xtab > 0) >= 20] > 0, 1, 0)
-## opticut results, note the cloglog link function
-#dol <- opticut(Y ~ stratum + lmoist + method, data=dolina$samp,
-#    strata=dolina$samp$mhab, dist="binomial:cloglog")
-dol <- opticut(Y, strata=dolina$samp$mhab, dist="binomial:cloglog")
+ddata <- dolina$samp[dolina$samp$method == "T",]
+Y <- dolina$xtab[dolina$samp$method == "T",]
+Y <- ifelse(Y[,colSums(Y > 0) >= 20] > 0, 1, 0)
+dol <- opticut(Y, strata=ddata$mhab, dist="binomial")
 
 ## parallel computing for uncertainty
 library(parallel)
@@ -15,6 +13,7 @@ object <- uncertainty(dol, type="multi", B=25, cl=cl)
 stopCluster(cl)
 
 
+strata.opticut <- opticut:::strata.opticut
 strata.uncertainty <-
 function (object, ...) {
     strata.opticut(object, ...)
@@ -23,29 +22,22 @@ function (object, ...) {
 bsmooth <- function (object, ...)
     UseMethod("bsmooth")
 
+bsmooth.uncertainty1 <- function (object, ...) {
+    bp <- bestpart(object)
+    t(t(bp) * object$mu1 + (1 - t(bp)) * object$mu0)
+}
 bsmooth.uncertainty <- function (object, ...) {
-    st <- as.character(strata(object))
-    out <- object$uncertainty
-    for (i in seq_len(length(out))) {
-        uc1 <- object$uncertainty[[i]]
-        bp1 <- bestpart(object$uncertainty[[i]])
-        m <- matrix(NA, nobs(object), ncol(bp1))
-        for (j in seq_len(nobs(object))) {
-            m[j,] <- bp1[st[j],] * uc1$mu1 + (1 - bp1[st[j],]) * uc1$mu0
-        }
-        out[[i]] <- m
-    }
-    out
+    lapply(object$uncertainty, bsmooth)
 }
 
-bs <- bsmooth(object)
-boxplot(rowMeans(bs[[1]]) ~ strata(object))
+yy <- mefa4::groupMeans(Y, 1, strata(object))
+boxplot(t(bsmooth(object$uncertainty[["pvic"]])), range=0)
+points(yy[,"pvic"], col=2, pch=4, cex=2)
 
 xx <- sapply(bs, rowMeans)
 xx <- mefa4::groupMeans(xx, 1, strata(object))
 heatmap(t(xx), scale="none", col=occolors()(25),
     distfun=function(x) dist(x, "manhattan"))
 
-yy <- mefa4::groupMeans(Y, 1, strata(object))
-plot(yy, xx);abline(0,1)
+plot(yy, xx, xlim=c(0,1), ylim=c(0,1));abline(0,1)
 
