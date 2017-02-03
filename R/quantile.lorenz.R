ed5fcf131d49ce204cd0471deea9fcc5c59a0508
Peter Solymos <psolymos@gmail.com>
2017-02-02 18:40:07 -0700

    bummer

--- R/quantile.lorenz.R
+++ R/quantile.lorenz.R
@@ -1,7 +1,7 @@
 quantile.lorenz <-
 function(x, probs = seq(0, 1, 0.25), ...)
 {
-    structure(sapply(probs, function(z) min(x[x[,"L"] >= z, "x"])),
+    structure(sapply(probs, function(z) min(x[x[,"p"] >= z, "x"])),
         names=paste0(format(100 * probs, trim=TRUE,
         digits = max(2L, getOption("digits"))), "%"))
 }
