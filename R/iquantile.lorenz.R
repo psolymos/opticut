c05a6e62aa4608ebd57ad1bc490c059c1a79b6df
Peter Solymos <psolymos@gmail.com>
2017-02-02 18:40:50 -0700

    cleanup

--- R/iquantile.lorenz.R
+++ R/iquantile.lorenz.R
@@ -5,7 +5,3 @@ function(x, values, ...)
         names=paste0(format(values, trim=TRUE,
         digits = max(2L, getOption("digits")))))
 }
-
-q <- seq(0, 1, 0.25)
-qq <- quantile(x, q)
-iquantile(x, qq)
