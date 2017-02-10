013133e95eae4a8df3b8d8524ffa0ad95fc52e16
Peter Solymos <psolymos@gmail.com>
2017-02-10 11:35:59 -0700

    Revert "NA passing behaviour at higher levels"
    This reverts commit edcc4db5ffbc6b6310b2bbd690210d113e2bccaa.

--- R/rankComb.R
+++ R/rankComb.R
@@ -9,7 +9,7 @@ function(Y, X, Z, dist="gaussian", collapse, ...)
         stop("length(Y) must equal length(Z)")
     if (missing(collapse))
         collapse <-  getOption("ocoptions")$collapse
-    Z0 <- model.matrix(~Z, model.frame(~Z, na.action=na.pass))
+    Z0 <- model.matrix(~Z)
     m <- .opticut1(Y, X, Z1=Z0[,-1L,drop=FALSE],
         linkinv=TRUE, dist=dist, ...)
     lc <- c(m$coef[1], m$coef[1] + m$coef[2:ncol(Z0)])
