b8357238aba306c691f8b7cab5112312de4f2da1
Peter Solymos <psolymos@gmail.com>
2017-02-27 22:21:21 -0700

    predict used internals

--- R/predict.multicut.R
+++ R/predict.multicut.R
@@ -17,17 +17,22 @@ function (object, gnew=NULL, xnew=NULL, ...)
             model.matrix(ff, xnew) else data.matrix(xnew)
         linkinv <- .opticut1(object$Y[,1L], X=object$X, Z1=NULL,
             dist=object$dist)$linkinv
+        Dist <- strsplit(object$dist, ":", fixed=TRUE)[[1L]][1L]
+        Link <- strsplit(object$dist, ":", fixed=TRUE)[[1L]][2L]
         g <- factor(gnew, levels(object$strata))
         if (any(is.na(X)) || any(is.na(g)))
             stop("new data must not have any NA")
+        Z <- model.matrix(~g)[,-1L,drop=FALSE]
         cf <- sapply(names(object$species), function(z) getMLE(object, z)$coef)
         fit <- matrix(0, nrow(X), ncol(cf))
         dimnames(fit) <- list(rownames(X), colnames(cf))
         for (i in seq_len(ncol(cf))) {
             ## change this --------- FIXME
             #gg <- ifelse(g %in% colnames(bp)[bp[i,] == 1], 1, 0)
-            XX <- cbind(model.matrix(~g), X[,-1L,drop=FALSE])
-            fit[,i] <- linkinv(drop(XX %*% cf[,i]))
+#            XX <- cbind(model.matrix(~g), X[,-1L,drop=FALSE])
+#            fit[,i] <- linkinv(drop(XX %*% cf[,i]))
+            fit[,i] <- .predict_dist(cf[,i],
+                dist=Dist, link=Link, X=X, Z=Z, linkinv=linkinv)
         }
     }
     fit
