9a714f2effe3b3fa2d6e65e032650b3939a83e46
Peter Solymos <psolymos@gmail.com>
2017-02-27 20:36:13 -0700

    throw error

--- R/fitted.opticut.R
+++ R/fitted.opticut.R
@@ -1,6 +1,10 @@
 fitted.opticut <-
 function (object, ...)
 {
+    if (is.function(object$dist))
+        stop("fitted values not available for custom distriutions")
+    if (!.opticut_dist(object$dist))
+        stop("distribution not recognized")
     fit <- sapply(bestmodel(object), fitted)
     rownames(fit) <- rownames(object$Y)
     fit
