51342d68d5c4ae63be269c0c7bf5110a05c7e06d
Peter Solymos <psolymos@gmail.com>
2017-02-28 22:35:46 -0700

    ref

--- R/getMLE.multicut.R
+++ R/getMLE.multicut.R
@@ -24,6 +24,11 @@ function(object, which, vcov=FALSE, ...)
         coef(m1) else object$species[[which]]$coefficients
     V <- if (vcov)
         vcov(m1) else NULL
+    if (Dist == "rsf") {
+        est <- c("(Intercept)"=0, est)
+        if (vcov)
+            V <- rbind("(Intercept)"=NA, cbind("(Intercept)"=NA, V))
+    }
     if (Dist %in% c("zip2", "zinb2")) {
         est <- est[c((length(est)-(K-1L)):(length(est)), 1:(length(est)-K))]
         if (vcov)
