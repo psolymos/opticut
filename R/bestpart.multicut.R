2c518f0225a90fa36a82090d6a893cc1c20306e2
Peter Solymos <psolymos@gmail.com>
2017-03-04 16:04:13 -0700

    spell it out

--- R/bestpart.multicut.R
+++ R/bestpart.multicut.R
@@ -4,6 +4,8 @@ function (object, pos_only=FALSE, ...)
 {
     fit <- fitted(object)
     lc <- t(apply(fit, 2, function(z) summary(lorenz(z))))
-    bp <- t(ifelse(t(fit) > lc[,"x(t)"], 1, 0))
+    xt <- lc[,"x(t)"]
+    bp <- t(ifelse(t(fit) > xt, 1, 0))
+    #attr(bp, "xt") <- xt
     bp
 }
