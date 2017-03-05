6938d331cf236f4c47036719262c5c679a5729f9
Peter Solymos <psolymos@gmail.com>
2017-03-05 13:59:57 -0700

    add Matrix as dependency

--- R/lorenz_cut1dot.R
+++ R/lorenz_cut1dot.R
@@ -1,6 +1,7 @@
 .lorenz_cut1 <-
 function(x, g, fix_fitted=FALSE)
 {
+    #requireNamespace(Matrix)
     sum_by <- function(x, by) {
         mm <- as(factor(by, levels=unique(by)), "sparseMatrix")
         cbind(x=as.numeric(mm %*% x), by=rowSums(mm))
