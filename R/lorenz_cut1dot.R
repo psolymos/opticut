c169bb8af333d49f188c8f8710c08bff96ad2a74
Peter Solymos <psolymos@gmail.com>
2017-03-05 14:03:13 -0700

    namespace

--- R/lorenz_cut1dot.R
+++ R/lorenz_cut1dot.R
@@ -1,7 +1,7 @@
 .lorenz_cut1 <-
 function(x, g, fix_fitted=FALSE)
 {
-    #requireNamespace(Matrix)
+    requireNamespace(Matrix, quietly=TRUE)
     sum_by <- function(x, by) {
         mm <- as(factor(by, levels=unique(by)), "sparseMatrix")
         cbind(x=as.numeric(mm %*% x), by=rowSums(mm))
