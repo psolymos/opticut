cb4458da0a868c4d16c3e895bd59e1f126a75786
Peter Solymos <psolymos@gmail.com>
2017-03-04 22:13:12 -0700

    need to include x(t)

--- R/bestpart.multicut.R
+++ R/bestpart.multicut.R
@@ -5,7 +5,7 @@ function (object, ...)
     fit <- fitted(object)
     lc <- t(apply(fit, 2, function(z) summary(lorenz(z))))
     xt <- lc[,"x(t)"]
-    out <- t(ifelse(t(fit) > xt, 1, 0))
+    out <- t(ifelse(t(fit) >= xt, 1, 0))
     #attr(bp, "xt") <- xt
     rownames(out) <- strata(object)
     out
