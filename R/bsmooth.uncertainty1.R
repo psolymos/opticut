318b3252bb3a1ce20df587ac236dde93fd969b63
Peter Solymos <psolymos@gmail.com>
2017-03-12 00:22:26 -0700

    fix

--- R/bsmooth.uncertainty1.R
+++ R/bsmooth.uncertainty1.R
@@ -1,12 +1,10 @@
 bsmooth.uncertainty1 <- function (object, ...) {
-    if (inherits(object, "uncertainty_opti")) {
+    if (inherits(object, "uncertainty1_opti"))
+        stop("bsmooth is not available for multicut based objects.")
+    if (inherits(object, "uncertainty1_opti")) {
         if (attr(object,"type") != "multi")
             stop("type='multi' required for bsmooth")
         bp <- bestpart(object)
-        out <- t(t(bp) * object$mu1 + (1 - t(bp)) * object$mu0)
+        return(t(t(bp) * object$mu1 + (1 - t(bp)) * object$mu0))
     }
-    if (inherits(object, "uncertainty_opti")) {
-        stop("bsmooth is not available for multicut based objects.")
-    }
-    out
 }
