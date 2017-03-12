8c17b8f3d6aa75d41b667269ffa471ecd3059900
Peter Solymos <psolymos@gmail.com>
2017-03-11 22:31:52 -0700

    print

--- R/print.uncertainty.R
+++ R/print.uncertainty.R
@@ -1,7 +1,11 @@
 print.uncertainty <-
 function(x, ...)
 {
-    cat("Multivariate opticut uncertainty results",
+    inherits(object, "uncertainty_opti")
+        cls <- "opticut"
+    inherits(object, "uncertainty_multi")
+        cls <- "multicut"
+    cat("Multivariate", cls, "uncertainty results",
         ", type = ", x$type, ", B = ", x$B, "\n\n", sep="")
     invisible(x)
 }
--- R/print.uncertainty1.R
+++ R/print.uncertainty1.R
@@ -1,7 +1,11 @@
 print.uncertainty1 <-
 function(x, ...)
 {
-    cat("Univariate opticut uncertainty results",
+    inherits(object, "uncertainty_opti")
+        cls <- "opticut"
+    inherits(object, "uncertainty_multi")
+        cls <- "multicut"
+    cat("Univariate", cls, "uncertainty results",
         ", type = ", attr(x, "type"), ", B = ", attr(x, "B"),
         "\n\n", sep="")
     print(summary(x), ...)
