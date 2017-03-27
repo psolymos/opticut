1e610509db04e29aac12f5a5d6f778a8b0e41887
Peter Solymos <psolymos@gmail.com>
2017-03-26 20:47:24 -0600

    dots etc

--- R/lcplot.multicut1.R
+++ R/lcplot.multicut1.R
@@ -13,12 +13,14 @@ bty="o", theme, ...)
     COL <- occolors(theme)(100)
     h0 <- ifelse(bp == 1, 0.5*x$I+0.5, 0.5*(1-x$I))
     ColID <- as.integer(pmin(pmax(1, floor(h0 * 100) + 1), 100))
-    plot(l, type="L", axes=FALSE, ann=FALSE)
+    plot(l, type="L", axes=FALSE, ann=FALSE, ...)
     for (i in seq_len(N)) {
         polygon(l[c(i, i+1, i+1, i), "p"],
             c(l[c(i, i+1), "L"], c(-1, -1)),
-            col=COL[ColID[i+1]], border=NA)
+            col=COL[ColID[i+1]], border="white", lwd=0.5)
     }
+    lines(l, col=1, lwd=2)
+    #axis(2, seq(0, 1, 0.25))
     axis(2)
     axis(1, 0.5 * (l[-1,"p"]+l[-(N+1),"p"]), rownames(l)[-1L])
     box(bty=bty)
