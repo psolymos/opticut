74a7e23c9f4bcc9a6127ecfdcdd23100aab67591
Peter Solymos <psolymos@gmail.com>
2017-04-01 01:20:42 -0600

    object$X needed for rspf

--- R/get_linkinvdot.R
+++ R/get_linkinvdot.R
@@ -1,3 +1,3 @@
 .get_linkinv <- function(object, ...) {
-    .opticut1(object$Y[,1L], dist=object$dist, ...)$linkinv
+    .opticut1(object$Y[,1L], X=object$X, Z1=NULL, dist=object$dist, ...)$linkinv
 }
