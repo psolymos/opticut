fe49cec2f6e1691ff4c767174d5935b10a091f61
Peter Solymos <psolymos@gmail.com>
2017-04-01 19:02:53 -0600

    lets binaries fully

--- R/multicut1.R
+++ R/multicut1.R
@@ -1,9 +1,6 @@
 ## Y is abundance vector
 ## X is model matrix for nuisance variables
 ## Z is factor
-##
-## does not store .lc_cut1 output because it might fail
-## leva that for downstream functions but store ingredients
 multicut1 <-
 function(Y, X, Z, dist="gaussian", sset=NULL, ...)
 {
@@ -27,22 +24,39 @@ function(Y, X, Z, dist="gaussian", sset=NULL, ...)
     res <- .opticut1(Y, X, Z1=Z0[,-1L,drop=FALSE],
         linkinv=TRUE, dist=dist, ...)
     cf <- res$coef
-    mulink <- c(cf[1], cf[1] + cf[2:ncol(Z0)])
-    mu <- res$linkinv(mulink)
+    gamma <- c(cf[1], cf[1] + cf[2:ncol(Z0)])
+    mu <- res$linkinv(gamma)
     names(mu) <- levels(Z)
+    K <- nlevels(Z)
+    mufix <- if (fix_fitted && any(mu < 0))
+        mu + abs(min(mu)) else mu
+    l <- lorenz(mufix, n)
+    bp <- structure(numeric(K), names=names(mu))
+    bp[rownames(l)[-seq_len(which.max(l[,"p"] - l[,"L"]))]] <- 1
+    bpvec <-
+    resbp <- .opticut1(Y, X, Z1=bpvec,
+        linkinv=TRUE, dist=dist, ...)
+    cfbp <- resbp$coef
     ll <- res$logLik
     scale <- getOption("ocoptions")$scale
     out <- list(
         null=res0$linkinv(res0$coef[1L]),
         mu=mu,
+        bestpart=bp,
+        beta0=cfbp[,1L],
+        beta1=cfbp[,2L],
+#        gamma0=cfbp[,1L],
+#        gamma1=cfbp[,1L] + cfbp[,2L],
+        I=beta2i(cfbp[,2L], scale=scale),
         #I=max(mulink)-min(mulink),
-        I=beta2i(max(mulink) - min(mulink), scale=scale),
+        #I=beta2i(max(mulink) - min(mulink), scale=scale),
         coefficients=cf,
         n=n,
         logL=res$logLik,
         logLR=res$logLik-res0$logLik)
     attr(out, "scale") <- scale
     attr(out, "logL_null") <- res0$logLik
+    attr(out, "logL_bp") <- resbp$logLik
     attr(out, "dist") <- if (is.function(dist))
         deparse(substitute(dist)) else .opticut_dist(dist, make_dist=TRUE)
     class(out) <- c("multicut1")
